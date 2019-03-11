e0aac0e49acd6dbbadde64287bcd450f42f3c359
Lucas Leite <lucasleitemm@hotmail.com>
2019-03-11 14:39:36 -0300

    load menu(from index) from db

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var con = require('./../inc/db');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Restaurante Maduro' });
+
+  con.query(`SELECT * FROM tb_menus ORDER BY title`, (err,results) => {
+
+    if(err){
+
+      console.log(err);
+
+    } else {
+
+      res.render('index', { 
+        title: 'Restaurante Maduro',
+        menus: results 
+      });
+    }
+
+  });
+  
 });
 
 module.exports = router;
